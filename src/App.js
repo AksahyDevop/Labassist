1ee5c7dd86b76fefa498d7c900e865c0d4803d03
AkshayDevop <ap0504766@gmail.com>
2024-12-06 23:33:20 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import LabServiceTable from './LabServiceTable';
+
+function App() {
+  return (
+    <div className="App">
+      <LabServiceTable/>
+    </div>
+  );
+}
+
+export default App;
